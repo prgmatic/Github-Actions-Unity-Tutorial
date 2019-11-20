9facbdd25b4e0f018f4b48934bc8f53962901737
Matthew Thompson <chatrat12@gmail.com>
2019-11-19 19:12:27 -0500

    Initial commit

---
+++ Assets/Scripts/BoxPile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class BoxPile : MonoBehaviour
+{
+    [SerializeField] private float _scale = 0.25f;
+    [SerializeField] private float _length = 0.25f;
+    [SerializeField] private int _rows = 10;
+    [SerializeField] private float _mass = 1f;
+    private Material _materal;
+
+    private void Start()
+    {
+        _materal = new Material(Shader.Find("Legacy Shaders/Diffuse"));
+
+        for (int row = 0; row < _rows; row++)
+        {
+            int boxCount = _rows - row;
+            for (int i = 0; i < boxCount; i++)
+            {
+                var left = Vector3.left * (boxCount * _scale * 0.5f + _scale * 0.5f);
+                var startPos = Vector3.up * (row * _scale + _scale * 0.5f);
+                startPos += left;
+                CreateCube(startPos + Vector3.right * _scale * i);
+            }
+        }
+    }
+
+    private void CreateCube(Vector3 position)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        var scale = Vector3.one * _scale;
+        scale.x = _length;
+        cube.transform.localScale = scale;
+        cube.transform.SetParent(transform);
+        cube.transform.localPosition = position;
+
+        var rb = cube.AddComponent<Rigidbody>();
+        rb.mass = _mass;
+
+        var renderer = cube.GetComponent<MeshRenderer>();
+        renderer.sharedMaterial = _materal;
+        renderer.material.color = Color.HSVToRGB(Random.value, 1, 1);
+    }
+}
---
+++ Assets/Scripts/ProjectileThrower.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ProjectileThrower : MonoBehaviour
+{
+    [SerializeField] private float _force = 100;
+    [SerializeField] private float _scale = 0.2f;
+    [SerializeField] private float _mass = 5f;
+    private Plane _plane;
+
+    private void Awake()
+    {
+        _plane = new Plane(-transform.forward, 2.5f);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            if (_plane.Raycast(ray, out distance))
+            {
+                var point = ray.GetPoint(distance);
+                ThrowProjectile(point);
+                Debug.Log(point);
+            }
+        }
+    }
+
+    private void ThrowProjectile(Vector3 targetPos)
+    {
+        var proj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        proj.transform.position = transform.position + Vector3.down * 0.25f;
+        proj.transform.localScale = Vector3.one * _scale;
+
+        var rb = proj.AddComponent<Rigidbody>();
+        rb.mass = _mass;
+
+        var direction = (targetPos - proj.transform.position).normalized;
+        var force = direction * _force;
+        rb.AddForce(force, ForceMode.VelocityChange);
+    }
+}
